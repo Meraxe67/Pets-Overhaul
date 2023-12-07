6b1de883600d3804910a699df08f6110363d4f81
Pearlie <bugrapearls@gmail.com>
2023-12-07 23:05:19 +0300

    Joja-Cola extra tooltip

--- Systems/ItemTooltipChanges.cs
+++ Systems/ItemTooltipChanges.cs
@@ -4,19 +4,23 @@ using Terraria;
 using Terraria.GameInput;
 using Terraria.Localization;
 using Terraria.ModLoader;
-
+using Terraria.ID;
 namespace PetsOverhaul.Systems
 {
     public class ItemTooltipChanges : GlobalItem
     {
         public override bool AppliesToEntity(Item entity, bool lateInstantiation)
         {
-            return ModContent.GetInstance<PetRegistry>().TerrariaPetItemIds.ContainsValue(entity.type);
+            return ModContent.GetInstance<PetRegistry>().TerrariaPetItemIds.ContainsValue(entity.type) || entity.type == ItemID.JojaCola;
         }
 
         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
         {
-            if (ModContent.GetInstance<Personalization>().TooltipsEnabledWithShift && !PlayerInput.Triggers.Current.KeyStatus[TriggerNames.Down])
+            if (item.type == ItemID.JojaCola)
+            {
+                tooltips.Add(new(Mod, "Tooltip0", Language.GetTextValue("Mods.PetsOverhaul.JojaCola")));
+            }
+            else if (ModContent.GetInstance<Personalization>().TooltipsEnabledWithShift && !PlayerInput.Triggers.Current.KeyStatus[TriggerNames.Down])
             {
                 tooltips.Add(new(Mod, "Tooltip0", Language.GetTextValue("Mods.PetsOverhaul.Config.TooltipShiftToggleInGame")));
             }
