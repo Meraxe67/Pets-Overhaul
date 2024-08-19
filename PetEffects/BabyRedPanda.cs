05b5e1787690263ced3f624d64754ec6a292b41a
Pearlie <bugrapearls@gmail.com>
2024-08-19 10:29:41 +0300

    Implemented Alert Ability

--- PetEffects/BabyRedPanda.cs
+++ PetEffects/BabyRedPanda.cs
@@ -3,6 +3,8 @@ using PetsOverhaul.Systems;
 using System;
 using System.Collections.Generic;
 using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent.UI;
 using Terraria.GameInput;
 using Terraria.ID;
 using Terraria.Localization;
@@ -13,21 +15,65 @@ namespace PetsOverhaul.PetEffects
     public sealed class BabyRedPanda : PetEffect
     {
         public override PetClasses PetClassPrimary => PetClasses.Offensive;
-        public override PetClasses PetClassSecondary => PetClasses.Harvesting;
-        public int aggroReduce = 500;
-        public float regularAtkSpd = 0.06f;
+        public override PetClasses PetClassSecondary => PetClasses.Utility;
+        public float regularAtkSpd = 0.05f;
         public float jungleBonusSpd = 0.04f;
         public int bambooChance = 50;
+        public int alertTime = 300;
+        public int alertCd = 1800;
+        public float alertMs = 0.03f;
+        public float alertAs = 0.015f;
+        public int alertAggro = 125;
+        public int alertRadius = 2400;
+        private int alertEnemies = 1;
+        public int alertEnemiesMax = 6;
+        private int alertTimer = 0;
+        public override void PreUpdate()
+        {
+            if (Pet.PetInUse(ItemID.BambooLeaf))
+            {
+                Pet.SetPetAbilityTimer(alertCd);
+                alertTimer--;
+                if (alertTimer <= 0)
+                {
+                    alertTimer = 0;
+                }
+            }
+        }
         public override void PostUpdateEquips()
         {
             if (Pet.PetInUseWithSwapCd(ItemID.BambooLeaf))
             {
-                Player.aggro -= aggroReduce;
-                Player.GetAttackSpeed<GenericDamageClass>() += regularAtkSpd;
-                if (Player.ZoneJungle)
+                Player.GetAttackSpeed<GenericDamageClass>() += regularAtkSpd + (Player.ZoneJungle ? jungleBonusSpd : 0);
+                if (alertTimer > 0)
                 {
-                    Player.GetAttackSpeed<GenericDamageClass>() += jungleBonusSpd;
+                    Player.GetAttackSpeed<GenericDamageClass>() += alertAs * alertEnemies;
+                    Player.moveSpeed += alertMs * alertEnemies;
+                    Player.aggro -= alertAggro * alertEnemies;
                 }
+            }
+        }
+        public override void ProcessTriggers(TriggersSet triggersSet)
+        {
+            if (Pet.timer <= 0 && Pet.PetInUseWithSwapCd(ItemID.BambooLeaf) && Keybinds.UsePetAbility.JustPressed)
+            {
+                SoundEngine.PlaySound(SoundID.Item37 with { Pitch = 1f }, Player.position);
+                EmoteBubble.MakePlayerEmote(Player, EmoteID.EmotionAlert);
+                alertEnemies = 1;
+                for (int i = 0; i < Main.maxNPCs; i++)
+                {
+                    NPC npc = Main.npc[i];
+                    if (npc.active && Player.Distance(npc.Center) < alertRadius)
+                    {
+                        alertEnemies++;
+                    }
+                }
+                if (alertEnemies > alertEnemiesMax)
+                {
+                    alertEnemies = alertEnemiesMax;
+                }
+                alertTimer = alertTime;
+                Pet.timer = Pet.timerMax;
             }
         }
         public override void Load()
@@ -64,9 +110,16 @@ namespace PetsOverhaul.PetEffects
             BabyRedPanda babyRedPanda = Main.LocalPlayer.GetModPlayer<BabyRedPanda>();
             tooltips.Add(new(Mod, "Tooltip0", Language.GetTextValue("Mods.PetsOverhaul.PetItemTooltips.BambooLeaf")
                 .Replace("<class>", PetColors.ClassText(babyRedPanda.PetClassPrimary, babyRedPanda.PetClassSecondary))
+                .Replace("<keybind>", Keybinds.UsePetAbility.GetAssignedKeys(GlobalPet.PlayerInputMode).Count > 0 ? Keybinds.UsePetAbility.GetAssignedKeys(GlobalPet.PlayerInputMode)[0] : $"[c/{Colors.RarityTrash.Hex3()}:{Language.GetTextValue("Mods.PetsOverhaul.KeybindMissing")}]")
+                .Replace("<alertAs>", Math.Round(babyRedPanda.alertAs * 100, 2).ToString())
+                .Replace("<alertMs>", Math.Round(babyRedPanda.alertMs * 100, 2).ToString())
+                .Replace("<alertAggro>", babyRedPanda.alertAggro.ToString())
+                .Replace("<alertRadius>", Math.Round(babyRedPanda.alertRadius / 16f, 2).ToString())
+                .Replace("<alertMax>", babyRedPanda.alertEnemiesMax.ToString())
+                .Replace("<alertDuration>", Math.Round(babyRedPanda.alertTime / 60f, 2).ToString())
+                .Replace("<alertCd>", Math.Round(babyRedPanda.alertCd / 60f, 2).ToString())
                 .Replace("<atkSpd>", Math.Round(babyRedPanda.regularAtkSpd * 100, 2).ToString())
                 .Replace("<jungleAtkSpd>", Math.Round(babyRedPanda.jungleBonusSpd * 100, 2).ToString())
-                .Replace("<aggro>", babyRedPanda.aggroReduce.ToString())
                 .Replace("<bambooChance>", babyRedPanda.bambooChance.ToString())
             ));
         }
