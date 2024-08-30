1ff5e2b50430f3baeca182545c6a573bd90f89a7
Pearlie <bugrapearls@gmail.com>
2024-08-30 12:14:52 +0300

    New turtle ability

--- PetEffects/Turtle.cs
+++ PetEffects/Turtle.cs
@@ -15,39 +15,35 @@ namespace PetsOverhaul.PetEffects
     {
         public override PetClasses PetClassPrimary => PetClasses.Defensive;
         public float moveSpd = 0.12f;
-        public float def = 0.12f;
-        public float kbResist = 0.9f;
+        public float def = 0.11f;
+        public float kbResist = 0.75f;
         public float dmgReduce = 0.04f;
         public int flatDef = 1;
+
         private int timer = 0;
-        public int shellUpTime = 480;
-        public float shellUpMult = 5f;
-        public int shellUpCd = 1800;
+        private int currentStacks = 0;
+        public int shellHardenDuration = 900;
+        public int shellHardenStacks = 5;
+        public int shellHardenCd = 2100;
+        public float dmgReduceShellHarden = 0.08f;
+        public float dmgReflect = 0.5f;
         public override void PreUpdate()
         {
             if (Pet.PetInUse(ItemID.Seaweed))
             {
-                Pet.SetPetAbilityTimer(shellUpCd);
+                Pet.SetPetAbilityTimer(shellHardenCd);
                 timer--;
                 if (timer <= 0)
+                {
+                    currentStacks = 0;
                     timer = 0;
+                }
             }
         }
         public override void PostUpdateEquips()
         {
             if (Pet.PetInUseWithSwapCd(ItemID.Seaweed))
             {
-                if (timer > 0)
-                {
-                    Player.statDefense += (int)Math.Ceiling(flatDef*shellUpMult);
-                    Player.statDefense *= def*shellUpMult + 1f;
-                    Player.moveSpeed -= moveSpd * shellUpMult;
-                    Player.GetDamage<GenericDamageClass>() -= dmgReduce*shellUpMult;
-                    Player.noFallDmg = true;
-                    Player.frozen = true;
-                    return;
-                }
-
                 Player.statDefense += flatDef;
                 Player.statDefense *= def+1f;
                 Player.moveSpeed -= moveSpd;
@@ -58,19 +54,35 @@ namespace PetsOverhaul.PetEffects
         {
             if (Pet.PetInUseWithSwapCd(ItemID.Seaweed))
             {
-                if (timer > 0)
+                if (currentStacks > 0)
                 {
                     modifiers.Knockback *= 0;
+                    modifiers.FinalDamage *= 1f - dmgReduceShellHarden;
                     return;
                 }
                 modifiers.Knockback *= 1f-kbResist;
+            }
+        }
+        public override void OnHurt(Player.HurtInfo info)
+        {
+            if (Pet.PetInUseWithSwapCd(ItemID.Seaweed) && currentStacks > 0)
+            {
+                if (info.DamageSource.TryGetCausingEntity(out Entity entity) && entity is NPC npc && npc.active == true && npc.immortal == false)
+                {
+                    NPC.HitInfo hit = new NPC.HitInfo() with { Crit = false, Damage = (int)(info.SourceDamage * dmgReflect), DamageType = DamageClass.Generic, HitDirection = npc.direction, Knockback = 1f };
+                    npc.StrikeNPC(hit);
+                    if (Main.netMode != NetmodeID.SinglePlayer)
+                        NetMessage.SendStrikeNPC(npc, hit);
+                }
+                currentStacks--;
             }
         }
         public override void ProcessTriggers(TriggersSet triggersSet)
         {
             if (Pet.timer <= 0 && Pet.PetInUseWithSwapCd(ItemID.Seaweed) && Keybinds.UsePetAbility.JustPressed)
             {
-                timer = shellUpTime;
+                timer = shellHardenDuration;
+                currentStacks = shellHardenStacks;
                 Pet.timer = Pet.timerMax;
             }
         }
@@ -92,7 +104,12 @@ namespace PetsOverhaul.PetEffects
             Turtle turtle = Main.LocalPlayer.GetModPlayer<Turtle>();
             tooltips.Add(new(Mod, "Tooltip0", Language.GetTextValue("Mods.PetsOverhaul.PetItemTooltips.Seaweed")
                 .Replace("<class>", PetTextsColors.ClassText(turtle.PetClassPrimary, turtle.PetClassSecondary))
-                        .Replace("<def>", turtle.def.ToString())
+                .Replace("<keybind>", PetTextsColors.KeybindText(Keybinds.UsePetAbility))
+                .Replace("<hitCount>", turtle.shellHardenStacks.ToString())
+                .Replace("<shellDuration>", Math.Round(turtle.shellHardenDuration/60f,2).ToString())
+                .Replace("<reducedDmg>", Math.Round(turtle.dmgReduceShellHarden*100,2).ToString())
+                .Replace("<reflect>", Math.Round(turtle.dmgReflect*100,2).ToString())
+                        .Replace("<def>", Math.Round(turtle.def*100,2).ToString())
                         .Replace("<kbResist>", Math.Round(turtle.kbResist*100,2).ToString())
                         .Replace("<flat>", turtle.flatDef.ToString())
                         .Replace("<moveSpd>", Math.Round(turtle.moveSpd * 100, 2).ToString())
