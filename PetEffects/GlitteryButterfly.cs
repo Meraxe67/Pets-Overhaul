ca012121450fec1c17f5d3346b5da68c6624670b
Pearlie <bugrapearls@gmail.com>
2024-09-18 23:16:43 +0300

    Balance

--- PetEffects/GlitteryButterfly.cs
+++ PetEffects/GlitteryButterfly.cs
@@ -13,8 +13,8 @@ namespace PetsOverhaul.PetEffects
     {
         public override PetClasses PetClassPrimary => PetClasses.Mobility;
         public int wingTime = 45;
-        public int bonusTimeIfExisting = 150;
-        public float healthPenalty = 0.08f;
+        public float currentWingPercIncr = 0.5f;
+        public float healthPenalty = 0.15f;
         public override void PostUpdateEquips()
         {
             if (Pet.PetInUseWithSwapCd(ItemID.BedazzledNectar))
@@ -22,21 +22,14 @@ namespace PetsOverhaul.PetEffects
                 if (Player.equippedWings == null)
                 {
                     Player.statLifeMax2 -= (int)(Player.statLifeMax2 * healthPenalty);
-                    Player.wings = 5;
-                    Player.wingsLogic = 5; //yüksek olduğunda oyuncu yürümekten acceleration almadıysa havada kımıldamak zorlaşıyor
+                    Player.wings = 5; //butterfly wings
+                    Player.wingsLogic = 5; //I don't exactly know what wingsLogic is.
                     Player.wingTimeMax = wingTime;
                     Player.noFallDmg = true;
                 }
                 else
                 {
-                    if (Player.equippedWings.type == ItemID.CreativeWings)
-                    {
-                        Player.wingTimeMax += wingTime;
-                    }
-                    else
-                    {
-                        Player.wingTimeMax += bonusTimeIfExisting;
-                    }
+                    Player.wingTimeMax += (int)(Player.wingTimeMax * currentWingPercIncr) + wingTime;
                 }
             }
         }
@@ -59,7 +52,7 @@ namespace PetsOverhaul.PetEffects
             tooltips.Add(new(Mod, "Tooltip0", Language.GetTextValue("Mods.PetsOverhaul.PetItemTooltips.BedazzledNectar")
                 .Replace("<class>", PetTextsColors.ClassText(glitteryButterfly.PetClassPrimary, glitteryButterfly.PetClassSecondary))
                         .Replace("<flight>", Math.Round(glitteryButterfly.wingTime / 60f, 2).ToString())
-                        .Replace("<bonusFlight>", Math.Round(glitteryButterfly.bonusTimeIfExisting / 60f, 2).ToString())
+                        .Replace("<percFlight>", Math.Round(glitteryButterfly.currentWingPercIncr * 100, 2).ToString())
                         .Replace("<healthNerf>", Math.Round(glitteryButterfly.healthPenalty * 100, 2).ToString())
                         ));
         }
